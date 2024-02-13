1496f28ebb3bb9169bc756bad46b3c4ff9771109
Olaifa-Olawale <olawale.favour10@gmail.com>
2024-02-13 10:16:43 -0600

    using app public directory

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Test');
-});
+// Serve static files from the 'public' directory
+app.use(express.static('public', { index: 'index.html' }));
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
