40a3a483b6f359b433dbacea3c7d935abed3344d
BlakeColeman <60763453+BlakeColeman@users.noreply.github.com>
2024-02-20 19:44:25 -0600

    Add files via upload

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,8 @@ const app = express();
 const sqlite3 = require('sqlite3'); 
 
 let db = new sqlite3.Database('./db/UofRCourseRater', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
+    if (err) 
+    {
       console.error(err.message);
     }
     console.log('Connected to the chinook database.');
--- public/js/login.js
+++ public/js/login.js
@@ -20,15 +20,16 @@ function ValidLogin(event)
 		emailMsg.innerHTML = "*Email address empty.";
 		checkResult = false;
 	}
-    else if(!emailCheck.test(emailInput))
-    {
-        emailMsg.innerHTML = "*Email address wrong format. example: username@uregina.ca";
+    	else if(!emailCheck.test(emailInput))
+   	 {
+        	emailMsg.innerHTML = "*Email address wrong format. example: username@uregina.ca";
 		checkResult = false;
-    }
-    else if (emailInput.length > 40){
-        emailMsg.innerHTML = "*Email address too long. Maximum is 60 characters.";
+   	 }
+   	 else if (emailInput.length > 40)
+	 {
+       		emailMsg.innerHTML = "*Email address too long. Maximum is 60 characters.";
 		checkResult = false;
-    }
+   	 }
     
 	// password
 	if(pswdInput == null || pswdInput == "")
@@ -36,15 +37,18 @@ function ValidLogin(event)
 		pswdMsg.innerHTML = "*Password is empty.";
 		checkResult = false;
 	}
-    else if(!pswdCheck.test(pswdInput)){
-        pswdMsg.innerHTML = "*Password must contain letters and at least one digit."
-        checkResult = false;
-    }
-    else if(pswdInput.length != 8){
-        pswdMsg.innerHTML = "*Password must be 8 Characters."
-        checkResult = false;
-    }
-	
+   	else if(!pswdCheck.test(pswdInput))
+	{
+        	pswdMsg.innerHTML = "*Password must contain letters and at least one digit."
+        	checkResult = false;
+    	}
+   	else if(pswdInput.length != 8)
+	{
+        	pswdMsg.innerHTML = "*Password must be 8 Characters."
+        	checkResult = false;
+    	}
+
+	//
 	if(checkResult == false)
 	{
 		event.preventDefault();
