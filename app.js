2f32d245c413cb06b0dd1e1ce8dedb2dd5759f6f
BlakeColeman <60763453+BlakeColeman@users.noreply.github.com>
2024-02-23 22:30:50 -0600

    Add files via upload
    fixed signup (for realsies this time)

--- app.js
+++ app.js
@@ -90,50 +90,59 @@ app.get('/createReview', (req, res) => {
 
 
 
-  app.post('/checkUsername', (req, res) => {
-    const { uname } = req.body;
-
+  app.post('/checkUsername', (req, res) => 
+  {
+    const {uname} = req.body;
+    console.log("Checking uname:",uname,"for availability");
+    var sql = 'SELECT * FROM users WHERE uname = ?';
     // Check if the username already exists in the database
-    db.get('SELECT * FROM users WHERE uname = ?', [uname], function(err, row) {
+    db.all(sql, [uname], (err, rows)=> 
+    {
         if (err) {
             // Handle any errors
             console.error(err.message);
             res.status(500).send('Internal Server Error');
             return;
         }
-
-        if (row) {
+        if (rows.length!=0) {
             // Username already exists, send a 400 Bad Request response
             res.status(400).send('Username already exists');
+            return;
         } else {
             // Username doesn't exist, send a 200 OK response
             res.status(200).send('Username available');
-        }
-    });
-});
-
-    app.post('/checkEmail',(req,res) => {
-        const { email } = req.body;
-        db.get('SELECT * FROM users WHERE uname = ?',[email], function(err, row){
-        if (err) {
-            // Hanlde any errors
-            console.error(err.message);
-            res.status(500).send('internal Server Error');
             return;
         }
-
-        if (row) {
-            //Email already registered, send a 400 Bad Request response
-            res.status(400).send('Email already in use'); 
-        }
-        else
-        {
-            //Email not registered, send a 200 OK response
-            res.status(200).send('Email available');
-        }
     });
-    
 });
+
+    app.post('/checkEmail',(req,res) => 
+    {
+        const {email} = req.body;
+        console.log("checking Email",email,"for availability");
+        var sql ='SELECT * FROM users WHERE email = ?';
+        db.all(sql,[email],(err, rows) =>
+        {
+            if (err) 
+            {
+                // Handle any errors
+                console.error(err.message);
+                res.status(500).send('internal Server Error');
+                return;
+            }
+            if (rows.length != 0) 
+            {
+                //Email already registered, send a 400 Bad Request response
+                res.status(400).send('Email already in use');
+            }
+            else
+            {
+                //Email not registered, send a 200 OK response
+                res.status(200).send('Email available');
+            }
+        })
+    
+    });
 
 // Start server
 app.listen(port, () => {
