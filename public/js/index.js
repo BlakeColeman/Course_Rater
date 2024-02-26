bb2b937c21b14a66479c5aab420f2242ecb8c874
ChrisHaus02 <chrishaus02@gmail.com>
2024-02-26 13:37:15 -0600

    Improvements to the search feature (does not allow user to submit if course does not exist)

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,38 @@
 // index.js
 
-// for the search function
+// Function to load data and handle response
 const load_data = function(query = '') {
-  fetch('/getCourse?cname=' + query)
-      .then(function(response) {
-          return response.json();
-      })
-      .then(function(responseData) {
-          var html = '';
-          if (responseData.length > 0) {
-              html += '<ul>';
-              responseData.forEach(function(course) {
-                  html += '<li>' + course.cname + '</li>';
-              });
-              html += '</ul>';
-          } else {
-              html += '<p>No courses found</p>';
-          }
-          document.getElementById('search_result').innerHTML = html;
-      })
-      .catch(function(error) {
-          console.error('Error fetching data:', error);
-          document.getElementById('search_result').innerHTML = '<p>No Course Found</p>';
-      });
+    fetch('/getCourse?cname=' + query)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(responseData) {
+            var html = '';
+            if (responseData.message) {
+                // Course not found, disable submit button
+                document.getElementById("mySubmit").disabled = true;
+            } else {
+                // Course found, enable submit button
+                document.getElementById("mySubmit").disabled = false;
+                html += '<ul>';
+                responseData.forEach(function(course) {
+                    html += '<li>' + course.cname + '</li>';
+                });
+                html += '</ul>';
+            }
+            document.getElementById('search_result').innerHTML = html;
+        })
+        .catch(function(error) {
+            console.error('Error fetching data:', error);
+            // Disable submit button if course entered does not exist
+            document.getElementById("mySubmit").disabled = true;
+            document.getElementById('search_result').innerHTML = '<p>Course not found</p>';
+        });
 };
 
+// Add event listener to search input
 var search_input = document.getElementById("search_input");
 search_input.addEventListener('input', function() {
-  var query = search_input.value;
-  load_data(query);
+    var query = search_input.value;
+    load_data(query);
 });
