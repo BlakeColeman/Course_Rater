28e7e91f81c53550ef07781e45a2693e52aac9f0
ChrisHaus02 <157054446+ChrisHaus02@users.noreply.github.com>
2024-02-27 15:11:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -50,6 +50,10 @@ app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'public','html', 'index.html'));
 });
 
+app.get('/index2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','html', 'index2.html'));
+});
+
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public','html', 'login.html'));
 });
