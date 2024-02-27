108ecba32f3a7f23a64aeb24f54c70b10ead77cb
Olaifa-Olawale <olawale.favour10@gmail.com>
2024-02-27 11:05:43 -0600

    created signup Route

--- app.js
+++ app.js
@@ -53,6 +53,8 @@ app.use(adminRoutes);
 // Student Routes
 app.use(studentRoutes);
 
+app.use(SignupRoutes);
+
 app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'public','html', 'index.html'));
 });
@@ -77,21 +79,6 @@ app.get('/createReview', (req, res) => {
     res.sendFile(path.join(__dirname, 'public','html', 'createReview.html'));
 });
 
- // Create new user
- app.post('/createUser', function(req,res){
-    console.log(req.body)
-    // SignupForm(req.body);
-    db.serialize(()=>{
-      db.run('INSERT INTO users(uname,email,pword) VALUES(?,?,?)', [req.body.uname,req.body.email, req.body.pword], function(err) {
-        if (err) 
-        {
-          return console.log(err.message);
-        }
-        console.log("New user has been added");
-        res.sendFile(path.join(__dirname, 'public','html', 'index2.html'));
-      });
-  });
-  }); 
 
   app.post('/createReviews',(req,res) =>
   {
@@ -113,59 +100,7 @@ app.get('/createReview', (req, res) => {
     })
 });
 
-  app.post('/checkUsername', (req, res) => 
-  {
-    const {uname} = req.body;
-    console.log("Checking uname:",uname,"for availability");
-    var sql = 'SELECT * FROM users WHERE uname = ?';
-    // Check if the username already exists in the database
-    db.all(sql, [uname], (err, rows)=> 
-    {
-        if (err) {
-            // Handle any errors
-            console.error(err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        if (rows.length!=0) {
-            // Username already exists, send a 400 Bad Request response
-            res.status(400).send('Username already exists');
-            return;
-        } else {
-            // Username doesn't exist, send a 200 OK response
-            res.status(200).send('Username available');
-            return;
-        }
-    });
-});
 
-    app.post('/checkEmail',(req,res) => 
-    {
-        const {email} = req.body;
-        console.log("checking Email",email,"for availability");
-        var sql ='SELECT * FROM users WHERE email = ?';
-        db.all(sql,[email],(err, rows) =>
-        {
-            if (err) 
-            {
-                // Handle any errors
-                console.error(err.message);
-                res.status(500).send('internal Server Error');
-                return;
-            }
-            if (rows.length != 0) 
-            {
-                //Email already registered, send a 400 Bad Request response
-                res.status(400).send('Email already in use');
-            }
-            else
-            {
-                //Email not registered, send a 200 OK response
-                res.status(200).send('Email available');
-            }
-        })
-    
-    });
 
     // search for a course
     app.get('/search', (req, res) => {
