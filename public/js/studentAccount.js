96a450fcdbacfa344040b39024bd210b1b10d731
ChrisHaus02 <chrishaus02@gmail.com>
2024-03-12 11:39:41 -0600

    Working on the delete feature for the student account page

--- public/js/studentAccount.js
+++ public/js/studentAccount.js
@@ -15,11 +15,14 @@ fetch('/userReviews')
         reviews.forEach(review => {
             const reviewElement = document.createElement('div');
             reviewElement.innerHTML = `
+                <article>
+                <button id="deleteReviewButton">Delete</button>
                 <p><b>Course Name:</b> ${review.cname}</p>
                 <p><b>Description:</b> ${review.content}</p>
                 <p><b>Grading:</b> ${review.grading}</p> 
                 <p><b>Additional Notes:</b> ${review.anotes}</p> 
                 <p><b>Rating:</b> ${review.crating}</p>
+                </article>
                 <br>
             `;
             userReviews.appendChild(reviewElement);
