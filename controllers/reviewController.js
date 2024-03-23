29ec989e8cb18eb712f1a9cbc545a7d49baf9bdb
BlakeColeman <60763453+BlakeColeman@users.noreply.github.com>
2024-03-23 00:01:20 -0600

    Implementing Database Facade

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,115 +1,31 @@
 //reviewController.js
-// accessed by the student and admin
-
-const sqlite3 = require('sqlite3').verbose(); 
+// accessed by the student and admin 
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const databaseModules = require('./database/databaseModules');
 
-// connect to the database
-function connectToDatabase() {
-    return new sqlite3.Database('./database/UofRCourseRater', (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-    });
-}
+db = databaseModules;
+
 
 // get the courses while searching
 const getCourses = (req, res) => {
-    const db = connectToDatabase();
-
-    const { cname } = req.query;
-
-    const sql = 'SELECT * FROM courses WHERE cname LIKE ? LIMIT 10'; // limit the number of courses shown
-    
-    const searchQuery = '%' + cname + '%';
-
-    db.all(sql, [searchQuery], (err, rows) => {
-        if (err) {
-          console.error(err.message);
-          res.status(500).send('Internal Server Error');
-          return;
-        }
-
-        if (rows.length === 0) 
-        {
-          res.status(404).send('Course does not exist');
-        }
-        else 
-        {
-          res.json(rows);
-        }
-    });
-    db.close()
+    db.getCourses(req,res);
 }
   
 // takes the user to the correct review page given search
 const reviews = (req, res) => {
-  
-    const db = connectToDatabase();
-    const { cname } = req.query; 
-    
-    const sql = 'SELECT * FROM courses WHERE cname LIKE ?';
-    
-    db.all(sql, [cname], (err, rows) => {
-        if (err) 
-        {
-            console.error(err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        
-        if (rows.length === 0) 
-        {
-            res.redirect('/index');
-        } 
-        else 
-        {
-            res.sendFile(path.join(__dirname,'../', 'public','view', 'reviewpage.html'));
-
-        }
-    });
-    db.close()
+    db.reviews(req,res);
 }
 
 // display reviews for a specific course
 const courseReview = (req, res) => {
-    const db = connectToDatabase();
-
-    const courseName = req.params.cname;
-    const sql = 'SELECT c.cname,r.review_id,r.uname,r.prof,r.content,r.grading,r.anotes,r.crating,r.rcreated FROM reviews r LEFT JOIN courses c on c.cid = r.cid WHERE cname LIKE ? ';
-
-    db.all(sql, [courseName], (err, rows) => {
-        if (err) {
-            console.error(err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        } else
-            res.json(rows);
-    })
-    db.close()
+    db.courseReview(req,res);
 }
 
 // Report a review
 const reportReview = (req, res) => {
-    const db = connectToDatabase();
-
-    const reviewId = req.params.reviewId;
-    
-    const sql = 'UPDATE reviews SET flags = 1 WHERE review_id = ?';
-
-    db.run(sql, [reviewId], (err) => {
-        if (err) {
-            console.error(err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        console.log(`Review ${reviewId} reported successfully`);
-        res.status(200)
-    });
-
-    db.close();
+    db.reportReview(req,res);
 }
 
 module.exports = 
--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,138 +1,38 @@
 //studentController.js
-
-const sqlite3 = require('sqlite3').verbose(); 
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const databaseModules = require('./database/databaseModules');
 
-// connect to the database
-function connectToDatabase() {
-    return new sqlite3.Database('./database/UofRCourseRater', (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-    });
-}
+const db = databaseModules;
 
 // display all reviews a user has made in their account page
-const userReviews = (req, res) => {
-    const db = connectToDatabase();
-
-    const uname = req.user.uname;  
-    const sql = 'SELECT c.cname,r.review_id,r.uname,r.prof,r.content,r.grading,r.anotes,r.crating,r.rcreated FROM reviews r LEFT JOIN courses c on c.cid = r.cid WHERE uname LIKE ? '; // limit the number of courses shown
-
-    db.all(sql, [uname], (err, rows)=> {
-        if (err) 
-        {
-            console.error(err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        else
-            res.json(rows);
-    })
-    db.close()
+const userReviews = (req, res) => 
+{
+    db.userReviews(req,res);
 }
 
 // display a single review to either delete or edit
-const reviewDetails = (req, res) => {
-    const db = connectToDatabase();
+const reviewDetails = (req, res) => 
+{
 
-    const reviewId = req.params.id;
-    const sql = 'SELECT c.cname,r.review_id,r.uname,r.prof,r.content,r.grading,r.anotes,r.crating,r.rcreated FROM reviews r LEFT JOIN courses c on c.cid = r.cid WHERE r.review_id = ?'; 
-
-    db.all(sql, [reviewId], (err, rows) => {
-        if (err) {
-            console.error(err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        } else 
-            res.json(rows);
-    })
-    db.close()
+    db.reviewDetails(req,res);
 }
 
 // creating a review
-const createReview = (req, res) => {
-
-    const db = connectToDatabase();
-    
-    const uname = req.user.uname;  
-    const cname = req.body.cname;
-    const currentDate = new Date().toISOString().split('T')[0];
-
-    const { prof, content, grading, anotes, rate } = req.body;
-    const sql = 'SELECT cid FROM courses WHERE cname LIKE ?';
-    db.get(sql, [cname], (err, row) => {
-        if (err) {
-            console.log(err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-
-        if (!row) {
-            console.log('Course not found'); 
-            res.status(404).send('Course not found');
-            return;
-        }
-
-        const cid = row.cid;
-        
-        const insertSql = 'INSERT INTO reviews (cid, prof, content, grading, anotes, crating, uname, rcreated) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        db.serialize(() => {
-            db.run(insertSql, [cid,prof,content, grading, anotes, rate, uname, currentDate], function(err) {
-                if (err) {
-                    console.log(err.message);
-                    res.status(500).send('Internal Server Error');
-                    return;
-                }
-                console.log('Review was inserted successfully');
-                res.redirect('/index'); 
-            });
-        });
-    });
+const createReview = (req, res) => 
+{
+    db.createReview(req,res);
 }
 
 // Allows the user logged in to edit one of their reviews
 const editReview = (req, res) => {
-    const db = connectToDatabase();
-
-    const {rid, prof, content, grading, anotes, rate } = req.body;
-    const updateSQL = 'UPDATE reviews SET prof = ?, content = ?, grading = ?, anotes = ?, crating =? WHERE review_id=? ;'
-    db.serialize(() => 
-    {
-        db.run(updateSQL, [prof, content, grading, anotes, rate, rid], function(err) 
-        {
-            if (err) 
-            {
-                console.log(err.message);
-                res.status(500).send('Internal Server Error');
-                return;
-            }
-            console.log('Review was edited successfully');
-            res.redirect('/account'); 
-        });
-    });
-    db.close()
+    db.editReview(req,res);
 }
 
 // deleting a review on the edit review page
 const deleteReview = (req, res) => {
-    const db = connectToDatabase();
-
-    const reviewId = req.params.id;
-    const sql = 'DELETE FROM reviews WHERE review_id = ?';
-
-    db.run(sql, [reviewId], function(err) {
-        if (err) {
-            console.error(err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        console.log(`Review ${reviewId} deleted successfully`);
-        res.sendStatus(200); // Send success response
-    });
-    db.close()
+    db.deleteReview(req,res);
 }
 
 module.exports = 
